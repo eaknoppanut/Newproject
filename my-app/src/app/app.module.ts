0cda2dfdc3e7724a20d7092914ae87436ffd8524
eaknoppanut <eaknoppanut@hotmail.com>
2017-11-06 20:56:33 +0700

    add bug

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -13,12 +13,13 @@ import { HomeComponent } from './components/home/home.component';
 import {ValidateService} from './services/validate.service';
 import {AuthService} from './services/auth.service';
 import {FlashMessagesModule} from 'angular2-flash-messages';
+import {AuthGuard} from './guards/auth.guard'
 
 const appRoutes: Routes=[
   {path:'', component:HomeComponent},
   {path:'register', component:RegisterComponent},
   {path:'login', component: LoginComponent},
-  {path:'profile',component: ProfileComponent}
+  {path:'profile',component: ProfileComponent , canActivate:[AuthGuard]}
 ]
 
 @NgModule({
@@ -37,7 +38,7 @@ const appRoutes: Routes=[
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService,AuthService],
+  providers: [ValidateService,AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
